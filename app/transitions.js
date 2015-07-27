d6c173d705f39c000a05c4bc82da3a2a193e6d37
santuan <smrcouto88@gmail.com>
2015-07-27 15:05:45 -0300

    Initial commit

---
+++ app/transitions.js
@@ -0,0 +1,30 @@
+export default function(){
+	this.transition(
+		this.fromRoute('index'),
+		this.toRoute('term.show'),
+		this.use('fade',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
+		this.reverse('fade',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]})
+	);
+
+	this.transition(
+		this.fromRoute('term.show'),
+		this.toRoute('post.show'),
+		this.use('toLeft',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
+		this.reverse('toRight',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]})
+	);
+
+
+	this.transition(
+		this.fromRoute('index'),
+		this.toRoute('post.show'),
+		this.use('fade',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
+		this.reverse('fade',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]})
+	);
+
+	
+	this.transition(
+	  this.withinRoute('term.show'),
+	  this.use('fade', { duration: 3000 })
+	);
+}
+
