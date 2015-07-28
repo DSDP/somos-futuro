7bb3c068f39c5f9561179e892f98651b4ae9fd26
Mbiondo <mbiondo@goblab.org>
2015-07-28 16:41:38 -0300

    Search tool

---
+++ app/components/search-tool.js
@@ -0,0 +1,30 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+	posts: [],
+	query: '',
+	searching: false,
+	searched: false,
+	interval: null,
+
+	searchChanged: function () {
+		var _this = this;
+		if (this.get('interval')) {
+			clearInterval(this.get('interval'));
+		} 
+		if (this.get('query') && this.get('query').length > 3) {
+			var interval = setInterval(function () {
+				_this.set('posts', []);
+				_this.set('searching', true);
+				_this.set('searched', false);
+				_this.get('store').find('post', {filter: {s: _this.get('query'), posts_per_page: 100}}).then(function (results) {
+					_this.set('posts', results);
+					_this.set('searching', false);
+					_this.set('searched', true);				
+				});		
+				clearInterval(_this.get('interval'));	
+			}, 500);
+			this.set('interval', interval);
+		}
+	}.observes('query'),
+});
