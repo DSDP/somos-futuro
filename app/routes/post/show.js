50ef6421d2e5800ef066d47432fbc68c548bfa75
apivetta <aguspivetta@gmail.com>
2015-08-06 18:32:30 -0300

    fix

--- app/routes/post/show.js
+++ app/routes/post/show.js
@@ -3,21 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model: function (params) {
       return Ember.RSVP.hash({
-          post: this.store.find('post', params.post_id),
-          term: this.get('store').find('post', params.post_id).then(function(data){
-          	if(data._data.terms.category[0].parent === null){
-          		return data._data.terms.category[0];
-          	} else {
-          		return data._data.terms.category[1];
-          	}
-          }),
-          subterm: this.get('store').find('post', params.post_id).then(function(data){
-          	if(data._data.terms.category[0].parent !== null){
-          		return data._data.terms.category[0];
-          	} else {
-          		return data._data.terms.category[1];
-          	}
-          })
+          post: this.store.find('post', params.post_id)
       });		
 	}
 });
