975771ef073afed6600e77f5fec1a927f6f62478
apivetta <aguspivetta@gmail.com>
2015-08-06 17:14:30 -0300

    terms y subterms en posts

--- app/routes/post/show.js
+++ app/routes/post/show.js
@@ -4,8 +4,19 @@ export default Ember.Route.extend({
 	model: function (params) {
       return Ember.RSVP.hash({
           post: this.store.find('post', params.post_id),
-          terms: this.store.find('post', params.post_id).then(function(data){
-          	return data.get("categories");
+          term: this.get('store').find('post', params.post_id).then(function(data){
+          	if(data._data.terms.category[0].parent === null){
+          		return data._data.terms.category[0];
+          	} else {
+          		return data._data.terms.category[1];
+          	}
+          }),
+          subterm: this.get('store').find('post', params.post_id).then(function(data){
+          	if(data._data.terms.category[0].parent !== null){
+          		return data._data.terms.category[0];
+          	} else {
+          		return data._data.terms.category[1];
+          	}
           })
       });		
 	}
