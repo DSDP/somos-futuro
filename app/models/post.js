275a7af5c5793543153cb4f8e6e4aba52eacbc9b
Agustin Pivetta <aguspivetta@gmail.com>
2015-08-07 01:11:40 -0300

    routing y navegacion

--- app/models/post.js
+++ app/models/post.js
@@ -1,4 +1,3 @@
-import Ember from 'ember';
 import PostModel from 'ember-cli-to-wp-theme/models/post';
 
 export default PostModel.extend({
--- app/routes/post/show.js
+++ app/routes/post/show.js
@@ -3,7 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model: function (params) {
       return Ember.RSVP.hash({
-          post: this.store.find('post', params.post_id)
+        post: this.get('store').find('post', {filter: {p: params.post_id}}).then(function(results){
+		  return results.get('firstObject');
+		})
       });		
 	}
 });
