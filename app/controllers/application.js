19d3f99057b419d611822e993122211106ff0d1c
santuan <smrcouto88@gmail.com>
2015-08-13 23:37:07 -0300

    se saco el volver del home, se solicita una imagen de menos tamaño en el post

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,5 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	isShowMenu: false
+	isShowMenu: false,
+	isHome: false,
+
+	getCurrentURL: function () {
+		this.set('isHome', false);
+
+		if (RegExp('index').test(this.get('currentPath')) || RegExp('festa').test(this.get('currentPath'))) {
+			this.set('isHome', true);
+		}
+		
+		}.observes('currentPath'),
 });
