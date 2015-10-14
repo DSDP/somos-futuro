8dd890f5db03dc8d156cbf16f7f971235326c935
santuan <smrcouto88@gmail.com>
2015-10-14 20:28:21 -0300

    se agrego el addon perfect-scroll

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
+	setupController: function (controller, model) {
+		this._super(controller, model);
+		controller.set('model', this.modelFor('application'));
+	},
 });
